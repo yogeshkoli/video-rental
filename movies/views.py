316fb314fce52a62de9d1606505c56016760eac9
ykoli <ykoli@ascllc.net>
2021-05-10 17:39:48 +0530

    Render view with provide list of recrods

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    movies = Movie.objects.all()
+
+    return render(request, 'index.html', {movies: movies})
