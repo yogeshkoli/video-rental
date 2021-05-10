4b9fcd41e98b8256f954f930557219e7515d87a9
ykoli <ykoli@ascllc.net>
2021-05-10 16:37:20 +0530

    customzed admin : listing movies

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Genre, Movie
 
-# Register your models here.
-admin.site.register(Genre)
-admin.site.register(Movie)
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class MovieAdmin(admin.ModelAdmin):
+    # fields = ('title', 'genre')
+    list_display = ('title', 'number_in_stock', 'daily_rate')
+    exclude = ('date_created', )
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
