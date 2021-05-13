c7c55ddc2ea111fe82378d41a05dc76394a13b54
ykoli <ykoli@ascllc.net>
2021-05-13 18:01:28 +0530

    render templates form the directory & the minor design changes

--- movies/views.py
+++ movies/views.py
@@ -5,4 +5,4 @@ from .models import Movie
 
 def index(request):
     movies = Movie.objects.all()
-    return render(request, 'index.html', {'movies': movies})
+    return render(request, 'movies/index.html', {'movies': movies})
