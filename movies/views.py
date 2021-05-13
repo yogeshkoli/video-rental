20ec43c8eec66ed01143cada098cba09e90c3589
ykoli <ykoli@ascllc.net>
2021-05-13 18:16:36 +0530

    New url with int parameter to get moive id and get the object from the database and display details

--- movies/views.py
+++ movies/views.py
@@ -6,3 +6,8 @@ from .models import Movie
 def index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
+
+
+def details(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    return render(request, 'movies/details.html', {'movie': movie})
