eb8b8cb7ac0d9dde86bb5e7c5b05d55ef46868e6
ykoli <ykoli@ascllc.net>
2021-05-13 18:22:48 +0530

    use dhango shortcuts to handel 404 exception

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 
@@ -9,8 +9,5 @@ def index(request):
 
 
 def details(request, movie_id):
-    try:
-        movie = Movie.objects.get(id=movie_id)
-        return render(request, 'movies/details.html', {'movie': movie})
-    except Movie.DoesNotExist:
-        raise Http404()
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/details.html', {'movie': movie})
